f35df070a853932e6fcd96bc5922f5f62ad2f48b
Christopher Bebry <invalidsyntax@gmail.com>
2016-02-13 02:21:33 -0500

    More refactoring and setup of usage plan

--- index.js
+++ index.js
@@ -4,65 +4,43 @@ var util = require('./lib/util')
   , q = require('q')
   , qfs = require('q-io/fs')
   , argv = require('yargs')
-      .usage('Usage: $0 -m [mode]')
+      .usage('Usage: $0 -m [mode] -s [service]')
+
+      .describe('s', 'Choose a service to work with')
+      .choices('s', ['system', 'apache', 'nginx', 'mysql'])
+      .alias('s', 'service')
+      //.default('s', 'system')
+
       .describe('m', 'Mode of operation')
-      .choices('m', ['apache', 'nginx', 'analysis'])
-      .demand(['m'])
+      .choices('m', ['analysis', 'backup'])
       .alias('m', 'mode')
+      .default('m', 'analysis')
+
+      .demand(['m', 's'])
       .argv
   ;
 
+
 if (argv.m === 'analysis') {
-    console.log('dir grabbing');
-    var env = process.env;
-    var home = env.HOME;
-    var user = env.LOGNAME || env.USER || env.LNAME || env.USERNAME;
-    var sudoUser = env.SUDO_USER;
-    var actualHome;
-
-    if (sudoUser === undefined) {
-      //TODO make usingRootAsSudo configurable
-      var usingRootAsSudo = false;
-      
-      if (usingRootAsSudo) {
-        actualHome = home || (process.getuid() === 0 ? '/root' : (user ? '/home/' + user : null));
-      } else {
-        actualHome = home || (user ? '/home/' + user : null);
-      }
-    } else {
-        actualHome = (sudoUser ? '/home/' + sudoUser : null);
+    if (argv.s === 'system') {
+        console.log('appStorageDirectory: ', util.getAppStorageDirectory());
+    } else if (argv.s === 'apache') {
+        q.all([
+            qfs.list('/etc/apache2/sites-available/'),
+            qfs.list('/etc/apache2/sites-enabled/'),
+        ]).then(function(results) {
+            console.log('Apache2 sites-available: ', results[0]);
+            console.log('Apache2 sites-enabled: ', results[1]);
+            console.log('Apache2 sites list are the same: ', util.unsortedCompareArray(results[0], results[1]));
+        });
+    } else if (argv.s === 'nginx') {
+        q.all([
+            qfs.list('/etc/nginx/sites-available/'),
+            qfs.list('/etc/nginx/sites-enabled/'),
+        ]).then(function(results) {
+            console.log('Nginx sites-available: ', results[0]);
+            console.log('Nginx sites-enabled: ', results[1]);
+            console.log('Nginx sites list are the same: ', util.unsortedCompareArray(results[0], results[1]));
+        });
     }
-
-    
-
-    console.log('home: ', home);
-    console.log('user: ', user);
-
-    console.log('actualHome: ', actualHome); 
-
-    console.log('sudoUser: ', sudoUser);
-}
-  
-if (argv.m === 'apache') {
-    q.all([
-        qfs.list('/etc/apache2/sites-available/'),
-        qfs.list('/etc/apache2/sites-enabled/'),
-    ]).then(function(results) {
-        console.log('Apache2 sites-available: ', results[0]);
-        console.log('Apache2 sites-enabled: ', results[1]);
-        console.log('Apache2 sites list are the same: ', util.unsortedCompareArray(results[0], results[1]));
-    });
-    
-}
-
-if (argv.m === 'nginx') {
-    q.all([
-        qfs.list('/etc/nginx/sites-available/'),
-        qfs.list('/etc/nginx/sites-enabled/'),
-    ]).then(function(results) {
-        console.log('Nginx sites-available: ', results[0]);
-        console.log('Nginx sites-enabled: ', results[1]);
-        console.log('Nginx sites list are the same: ', util.unsortedCompareArray(results[0], results[1]));
-    });
-    
 }
